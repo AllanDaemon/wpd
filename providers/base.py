b85dfb2e0a5b14dc46523a4f8abbfaf0e9ba4778
Allan Daemon <rea.aft@gmail.com>
2020-09-23 15:51:08 -0300

    provider.base

---
+++ providers/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+from pathlib import Path
+
+
+CACHE_DIR = Path('../cache')
+
+log = print
+
+class ImageBase:
+	...
+
+
+class ProviderBase:
+	...
+
