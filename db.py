88e682350ab9159bd899053d40d5284f868e457e
Allan Daemon <rea.aft@gmail.com>
2020-09-25 16:47:45 -0300

    db: update ApodStatus

--- db.py
+++ db.py
@@ -16,8 +16,8 @@ class BaseModel(Model):
 class ApodStatus(BaseModel):
 	date = DateField(primary_key=True)
 	f_name = CharField(unique=True)
-	status = CharField(choices=['UNPROCESSED', 'OK', 'VERTICAL', 'IFRAME', 'OBJECT', 'EMBED', 'APPLET', 'ERROR'])
-	status_int = IntegerField(choices=[0, 1, 2, 10, 11, 12, 13, 100])
+	status = CharField(choices=['UNPROCESSED', 'OK', 'VERTICAL', 'OLD', 'IFRAME', 'OBJECT', 'EMBED', 'APPLET', 'ERROR'])
+	status_int = IntegerField(choices=[0, 1, 2, 3, 10, 11, 12, 13, 100])
 
 assert db.connect()
 
