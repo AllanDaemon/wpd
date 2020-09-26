5bfee49548fe3c3ddc541e5937f1199c17777003
Allan Daemon <rea.aft@gmail.com>
2020-09-26 20:41:25 -0300

    GUI: Create GTK main module

---
+++ gui.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class MainWindow(Gtk.Window):
+
+	def __init__(self):
+		super().__init__()
+		self.connect("destroy", Gtk.main_quit)
+
+
+if __name__ == "__main__":
+	main_win = MainWindow()
+	main_win.show_all()
+	Gtk.main()
