6ffba135bd9835548a64e402ca74e03f434650a3
Allan Daemon <rea.aft@gmail.com>
2020-09-26 22:12:04 -0300

    GUI: main and header bar work

--- widgets/header_bar.py
+++ widgets/header_bar.py
@@ -6,14 +6,20 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+from widgets.source_chooser import SourceChooserWidget
+
 
 
 class MainHeaderBarWidget(Gtk.HeaderBar):
 
 	def __init__(self, *args, **kwargs):
+		kwargs['title'] = "WPD"
+		kwargs['show_close_button'] = True
 		super().__init__(*args, **kwargs)
 	
 		self.init_ui()
 
 	def init_ui(self):
-		...
+		self.src_ch = SourceChooserWidget()
+		self.pack_start(Gtk.Label.new("Source"))
+		self.pack_start(self.src_ch)
--- widgets/main_win.py
+++ widgets/main_win.py
@@ -14,6 +14,9 @@ from widgets.gallery import GalleryWidget
 class MainWindow(Gtk.Window):
 
 	def __init__(self, *args, **kwargs):
+		kwargs['title'] = "WPD"
+		kwargs['default_width'] = 900
+		kwargs['default_height'] = 500
 		super().__init__(*args, **kwargs)
 		self.connect("destroy", Gtk.main_quit)
 	
@@ -22,3 +25,6 @@ class MainWindow(Gtk.Window):
 	def init_ui(self):
 		hb = MainHeaderBarWidget()
 		gallery = GalleryWidget()
+
+		self.set_titlebar(hb)
+		self.add(gallery)
