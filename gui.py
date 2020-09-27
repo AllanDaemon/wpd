5eb2ee5c12ae46e235d7f033bb783958a681e2ad
Allan Daemon <rea.aft@gmail.com>
2020-09-27 00:45:16 -0300

    GUI: set app font size (app css)

--- gui.py
+++ gui.py
@@ -4,11 +4,34 @@ from __future__ import annotations
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk	# type: ignore
 
 from widgets.main_win import MainWindow
 
-if __name__ == "__main__":
+
+# default font-size: 10pt;
+app_css = b'* {font-size: 12pt;}'
+
+def main(threaded=True):
 	main_win = MainWindow()
 	main_win.show_all()
-	Gtk.main()
+
+	style_prov = Gtk.CssProvider()
+	assert style_prov.load_from_data(app_css)
+	Gtk.StyleContext.add_provider_for_screen(
+		Gdk.Screen.get_default(),
+		style_prov,
+		Gtk.STYLE_PROVIDER_PRIORITY_USER
+	)
+
+	
+	if not threaded:
+		Gtk.main()
+	else:
+		from threading import Thread
+		t = Thread(name='GTK Thread', target=Gtk.main)
+		t.start()
+
+
+if __name__ == "__main__":
+	main()
